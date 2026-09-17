b55d6e8efcb4b302299dafd8c0f9033a9e681a87
Atieh Armin <atieharmin78@gmail.com>
2026-09-16 23:09:43 -0400

    Update: valid-sudoku - submission-5

--- Data Structures & Algorithms/valid-sudoku/submission-5.py
+++ Data Structures & Algorithms/valid-sudoku/submission-5.py
@@ -1,19 +1,36 @@
-import numpy as np
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        board = np.array(board)
-        for row in board:
-            filled_row = row[row != "."]
-            if len(set(filled_row)) != len(filled_row):
+        for i in range(9):
+            row = list(filter(lambda x: x != ".", board[i]))
+            if len(row) != len(set(row)):
                 return False
-        columns = board.T
-        for column in columns:
-            filled_column = column[column != "."]
-            if len(set(filled_column)) != len(filled_column):
+        columns = [list(row) for row in zip(*board)]
+        for i in range(9):
+            column = list(filter(lambda x: x != ".", columns[i]))
+            if len(column) != len(set(column)):
                 return False
-        squares = board.reshape(3,3,3,3).swapaxes(1,2).reshape(9, 9)
-        for square in squares:
-            filled_square = square[square != "."]
-            if len(set(filled_square)) != len(filled_square):
+        for i in range(0,9,3):
+            for j in range(0,9,3):
+                square = []
+                square.append(board[i][j])
+                square.append(board[i+1][j])
+                square.append(board[i+2][j])
+                square = list(filter(lambda x: x != ".", square))
+                if len(square) != len(set(square)):
+                    return False
+                square.append(board[i][j+1])
+                square.append(board[i+1][j+1])
+                square.append(board[i+2][j+1])
+                square = list(filter(lambda x: x != ".", square))
+                if len(square) != len(set(square)):
+                    return False
+                square.append(board[i][j+2])
+                square.append(board[i+1][j+2])
+                square.append(board[i+2][j+2])
+                square = list(filter(lambda x: x != ".", square))
+                if len(square) != len(set(square)):
                     return False
         return True
+        
+         
+        
